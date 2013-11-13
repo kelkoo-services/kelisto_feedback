b5644b42ba29ca1dcb92a3cf75ce0456e6e642c8
Pablico <pablo.molinacandel@gmail.com>
2013-11-13 12:44:50 +0100

    handle in case email is not defined

--- lib/kelisto_feedback/helper.rb
+++ lib/kelisto_feedback/helper.rb
@@ -1,7 +1,7 @@
 module KelistoFeedback
   module ViewHelpers
     def kelisto_feedback
-      @user_email = current_user.email if current_user.present?
+      @user_email = current_user.email if current_user.present? && current_user.respond_to?(:email)
       
       content_tag(:div, nil, id: "feedback-handle") +
       render('feedbacks/feedbox')
